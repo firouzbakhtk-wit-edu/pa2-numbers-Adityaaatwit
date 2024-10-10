7cd2bbdc18d70d9869f9ffd03c08ff6fcb3a4473
AdityaAditya <adityaa@wit.edu>
2024-10-10 10:34:07 -0400

    Assignment PA2

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,86 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
+		
 		// TODO: write your code here
-	}
+		        Scanner input = new Scanner(System.in);
+		        
+		        System.out.print("Enter five whole numbers: ");
+		        int num1 = input.nextInt();
+		        int num2 = input.nextInt();
+		        int num3 = input.nextInt();
+		        int num4 = input.nextInt();
+		        int num5 = input.nextInt();
+		        
+		        
+		        int sumPositive = 0;
+		        int sumNonPositive = 0;
+		        int sumAll = num1 + num2 + num3 + num4 + num5;
+		        
+		        int countPositive = 0;
+		        int countNonPositive = 0;
 
+		       
+		        if (num1 > 0) {
+		            sumPositive += num1;
+		            countPositive++;
+		        } else {
+		            sumNonPositive += num1;
+		            countNonPositive++;
+		        }
+
+		        if (num2 > 0) {
+		            sumPositive += num2;
+		            countPositive++;
+		        } else {
+		            sumNonPositive += num2;
+		            countNonPositive++;
+		        }
+
+		        if (num3 > 0) {
+		            sumPositive += num3;
+		            countPositive++;
+		        } else {
+		            sumNonPositive += num3;
+		            countNonPositive++;
+		        }
+
+		        if (num4 > 0) {
+		            sumPositive += num4;
+		            countPositive++;
+		        } else {
+		            sumNonPositive += num4;
+		            countNonPositive++;
+		        }
+
+		        if (num5 > 0) {
+		            sumPositive += num5;
+		            countPositive++;
+		        } else {
+		            sumNonPositive += num5;
+		            countNonPositive++;
+		        }
+
+		        
+		        double averagePositive = countPositive > 0 ? (double) sumPositive / countPositive : 0.0;
+		        double averageNonPositive = countNonPositive > 0 ? (double) sumNonPositive / countNonPositive : 0.0;
+		        double averageAll = (double) sumAll / 5;
+
+		        System.out.println("The sum of the " + countPositive + " positive numbers: " + sumPositive);
+		        System.out.println("The sum of the " + countNonPositive + " non-positive numbers: " + sumNonPositive);
+		        System.out.println("The sum of the 5 numbers: " + sumAll);
+		        System.out.printf("The average of the %d positive numbers: %.2f%n", countPositive, averagePositive);
+		        System.out.printf("The average of the %d non-positive numbers: %.2f%n", countNonPositive, averageNonPositive);
+		        System.out.printf("The average of the 5 numbers: %.2f%n", averageAll);
+		        
+		}
 }
+
+
+
+
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,42 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value of a,b,and c :");
+		
+		int a= input.nextInt();
+		int b= input.nextInt();
+		int c= input.nextInt();
+		
+		double d= (b*b)-(4*a*c);
+		 System.out.printf("The value of discriminant is: %.2f%n", d);
+		
+		double sqrtOfD= Math.sqrt(d);
+		double d1= -b/(2*a);
+		double d2 = ((-b)+(sqrtOfD))/(2*a);
+		double d3 = ((-b)-(sqrtOfD))/(2*a);
+		
+		if(d<0) {
+			System.out.println("Roots are imaginary");
+			return;
+		}else if(d==0) {
+			System.out.printf("There is only one root: %.2f%n", d1);
+			return;
+		}else {
+			if(d2>d3) {
+				System.out.printf("Roots: %.2f, %.2f%n", d3, d2);
+			}else {
+				System.out.printf("Roots: %.2f, %.2f%n", d2, d3);
+				
+			}
+		}
+		
+		
 	}
 
 }
